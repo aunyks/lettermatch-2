c72da0ce54485c694f878ed7af0456fcd6af759d
Gerald Nash <me@aunyks.com>
2020-05-09 01:36:27 -0400

    better thanks page

--- pages/thanks.js
+++ pages/thanks.js
@@ -14,7 +14,7 @@ export default function ThanksPage() {
           </h1>
           <div className="my-3 lg:my-5 lg:px-64 lg:text-justify">
             <p className="my-4">
-              We appreciate your order! You'll receive an email with your receipt very soon.
+              We appreciate your order! You'll receive an email with your receipt very soon. In the meantime, follow us on <a className="underline" href="https://www.instagram.com/mezclaxyz">Instagram</a>, <a className="underline" href="https://twitter.com/mezclaxyz">Twitter</a> and <a className="underline" href="https://www.facebook.com/mezclaxyz">Facebook</a> @mezclaxyz.
             </p>
           </div>
         </div>
