6913a5cb88fb0d48ec8a92108a796f228a485d61
Gerald Nash <me@aunyks.com>
2020-05-23 23:33:42 -0400

    Add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-G4SXLPN72R"></script>
+          <script defer>
+            {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag(\`js\`, new Date());
+
+      gtag(\`config\`, \`G-G4SXLPN72R\`);
+    `}
+          </script>
+        </Head>
         <body className="font-sans h-full w-full">
           <Main />
           <NextScript />
