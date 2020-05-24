f08c70917a8efa8929c7e90131e9ff9eee425563
Gerald Nash <me@aunyks.com>
2020-05-24 11:55:13 -0400

    only do analytics in prod

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const isProd = process.env.ENV_LEVEL === 'production'
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-G4SXLPN72R"></script>
-          <script defer>
-            {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
-      gtag(\`js\`, new Date());
+          {isProd && (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-G4SXLPN72R"></script>
+              <script defer>
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag() { dataLayer.push(arguments); }
+                  gtag(\`js\`, new Date());
 
-      gtag(\`config\`, \`G-G4SXLPN72R\`);
-    `}
-          </script>
+                  gtag(\`config\`, \`G-G4SXLPN72R\`);
+                `}
+              </script>
+            </>
+          )}
         </Head>
         <body className="font-sans h-full w-full">
           <Main />
